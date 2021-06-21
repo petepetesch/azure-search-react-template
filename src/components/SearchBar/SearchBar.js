46e10eb3ce9c24acddefc0aea04f6ce850fb1113
Peter Petesch <petepetesch@hotmail.com>
2021-06-21 13:47:01 -0700

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -78,7 +78,7 @@ export default function SearchBar(props) {
                         type="text" 
                         id="search-box" 
                         className="form-control rounded-0" 
-                        placeholder="What are you looking for?" 
+                        placeholder="What uvoc data are you looking for?" 
                         onChange={onChangeHandler} 
                         defaultValue={props.q}
                         onBlur={() => setShowSuggestions(false)}
