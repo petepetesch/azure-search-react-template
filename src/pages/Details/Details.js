8b17489ae600e54a0bb4fbeb6a047c13d4f6796f
Peter Petesch <petepetesch@hotmail.com>
2021-11-10 14:58:58 -0800

    Update Details.js
    fix 3

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -5,6 +5,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from 'axios';
 
 import "./Details.css";
+var PBIlink = "https://msit.powerbi.com/groups/28d98cd5-edc2-4e72-a5fa-ca18910196be/reports/fbe61164-ef7b-4c8b-a908-bcf4f2d13635/ReportSection2f347367692ad00a8703?filter=Feature%2FFeatureID%20eq%20%27";
 
 export default function Details() {
 
